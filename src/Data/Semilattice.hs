0c9b1f59c5caeda9bbe43962a174e235db81ee85
Rob Rix <rob.rix@me.com>
2017-10-19 20:36:24 -0400

    :fire: the default definitions.

--- src/Data/Semilattice.hs
+++ src/Data/Semilattice.hs
@@ -1,12 +1,7 @@
-{-# LANGUAGE DefaultSignatures #-}
 module Data.Semilattice where
 
 -- | A (bounded) join semilattice is a idempotent commutative monoid.
 class JoinSemilattice s where
   bottom :: s
-  default bottom :: Monoid s => s
-  bottom = mempty
 
   (\/) :: s -> s -> s
-  default (\/) :: Monoid s => s -> s -> s
-  (\/) = mappend
