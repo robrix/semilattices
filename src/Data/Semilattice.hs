20a13edaeb1087cc5b3d8aa67197d06024b33622
Rob Rix <rob.rix@me.com>
2017-10-19 20:35:00 -0400

    :memo: JoinSemilattice.

--- src/Data/Semilattice.hs
+++ src/Data/Semilattice.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DefaultSignatures #-}
 module Data.Semilattice where
 
+-- | A (bounded) join semilattice is a idempotent commutative monoid.
 class JoinSemilattice s where
   bottom :: s
   default bottom :: Monoid s => s
