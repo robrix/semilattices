fccd6e14dc8c6a87d5068bce44ec17f0cb7e6f66
Rob Rix <rob.rix@me.com>
2018-03-16 20:57:53 -0400

    Test that Set join is commutative.

--- src/Data/Join.hs
+++ src/Data/Join.hs
@@ -56,6 +56,9 @@ instance Ord a => Join (Max a) where
 --
 --   Associativity:
 --   prop> \ a b c -> a \/ (b \/ c) == (a \/ b) \/ (c :: Set.Set Char)
+--
+--   Commutativity:
+--   prop> \ a b -> a \/ b == b \/ (a :: Set.Set Char)
 instance Ord a => Join (Set.Set a) where
   (\/) = Set.union
 
