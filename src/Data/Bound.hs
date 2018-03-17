40dc69a3e40c08fa4514ff0f9e15a14499d542b5
Rob Rix <rob.rix@me.com>
2018-03-17 08:46:04 -0400

    Simplify the instances for Bound.

--- src/Data/Bound.hs
+++ src/Data/Bound.hs
@@ -7,8 +7,5 @@ import Data.Upper
 newtype Bound a = Bound { getBound :: a }
   deriving (Bounded, Enum, Eq, Foldable, Functor, Num, Ord, Read, Show, Traversable)
 
-instance Bounded a => Lower (Bound a) where
-  bottom = minBound
-
-instance Bounded a => Upper (Bound a) where
-  top = maxBound
+instance Bounded a => Lower (Bound a)
+instance Bounded a => Upper (Bound a)
