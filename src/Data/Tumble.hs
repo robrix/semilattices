40a231e3473c1eafcf0a0815ccab6d34616d4749
Rob Rix <rob.rix@me.com>
2018-03-16 20:02:04 -0400

    :fire: some pointless instances.

--- src/Data/Tumble.hs
+++ src/Data/Tumble.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Data.Tumble where
 
-import Control.Applicative
-import Control.Monad.Fix
-import Data.Coerce
 import Data.Join
 import Data.Lower
 import Data.Meet
@@ -11,20 +8,6 @@ import Data.Upper
 
 newtype Tumble a = Tumble { getTumble :: a }
   deriving (Enum, Eq, Foldable, Functor, Num, Ord, Read, Show, Traversable)
-
-instance Applicative Tumble where
-  pure = Tumble
-  a <* _ = a
-  _ *> a = a
-  (<*>) = coerce
-  liftA2 = coerce
-
-instance Monad Tumble where
-  (>>) = (*>)
-  Tumble a >>= f = f a
-
-instance MonadFix Tumble where
-  mfix f = fix (f . getTumble)
 
 instance Join a => Meet (Tumble a) where
   Tumble a /\ Tumble b = Tumble (a \/ b)
