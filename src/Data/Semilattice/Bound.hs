95e62225cdf3495529c3edf4f5ef287a358bb78d
Rob Rix <rob.rix@me.com>
2018-05-28 20:16:35 -0400

    :memo: Bound.

--- src/Data/Semilattice/Bound.hs
+++ src/Data/Semilattice/Bound.hs
@@ -5,6 +5,7 @@ module Data.Semilattice.Bound where
 import Data.Semilattice.Lower
 import Data.Semilattice.Upper
 
+-- | A convenience bridging 'Bounded' to 'Lower' and 'Upper'.
 newtype Bound a = Bound { getBound :: a }
   deriving (Bounded, Enum, Eq, Foldable, Functor, Num, Ord, Read, Show, Traversable)
 
